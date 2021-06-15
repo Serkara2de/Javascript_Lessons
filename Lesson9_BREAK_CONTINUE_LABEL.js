ec79e149f4cd847fe6f9f8e27496ecafdd86e65b
Serkan <serkara2de@hotmail.com>
2021-06-16 00:41:04 +0200

    break, continue, break label edded

--- Lesson9_BREAK_CONTINUE_LABEL.js
+++ Lesson9_BREAK_CONTINUE_LABEL.js
@@ -14,3 +14,19 @@ for (var i = 0; i < 10; i++) {
     }
     console.log(i)
 }
+
+console.log("=====break label=====")
+
+//00, 01...55
+loop1:
+for(var i=0; i<5;i++){
+    loop2:
+    for(var j=0;j<5;j++){
+        console.log(i+","+j)
+        if(i==3 && j==3){
+            break loop1; //loop1'i durdurur.Normal break sadece loop2 yi durdururu loop1 kaldığı yerden loop2 ile devam eder.
+            
+        }
+    }
+
+}
