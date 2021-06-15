93ccfbdf39d2f7608fa8ea803ff5d260bda089a3
Serkan <serkara2de@hotmail.com>
2021-06-15 23:25:26 +0200

    IfELSE

--- Lesson5_IF_Statements.js
+++ Lesson5_IF_Statements.js
@@ -31,3 +31,49 @@ if(books<20){
 }
 console.log(books);
 
+
+//IF ELSE Statements
+
+/*var isTheWeatherNice = false;
+
+if(isTheWeatherNice){
+    console.log("Lets go swimming")
+}else{
+    console.log("Stay at home and watch TV")
+}
+console.log("================")
+
+var benutzerName, password, inputBenutzerName, inputPassword;
+
+benutzerName="Mattihas"
+password="gehalt.de"
+
+inputBenutzerName = prompt("Enter benutzername");
+inputPassword= prompt("Enter password");
+
+if(benutzerName==inputBenutzerName && password==inputPassword){
+    console.log("Acces granted")
+    console.log("Welcome "+benutzerName)
+}else{
+    console.log("Benutzername oder passwort ist nicht korrekt!")
+}
+*/
+
+//IF ELSE IF ELSE Statement
+
+var score;
+score= parseInt("60");
+
+if(score>90){
+    console.log("A")
+} else if(score > 80 ) {
+    console.log("B")
+} else if (score > 70 ) {
+    console.log("C")
+} else if (score > 60 ) {
+    console.log("D")
+} else {
+    console.log("F")
+}
+
+
