d7b3f3b50e318ad8fbfacd5f5c71b786348fc9c5
Serkan <serkara2de@hotmail.com>
2021-06-15 23:37:19 +0200

    For Loop added

---
+++ Lesson6_FOR_Loop.js
@@ -0,0 +1,17 @@
+
+var counter=0;
+for(var i=0; i<5;i++){
+    counter++;
+    console.log(counter+ '-'+" Hello World "+i);
+    
+}
+
+console.log("==============")
+
+// print out odd numbers until 50
+
+for(var i=0;i<=50;i++){
+    if(i%2==1){
+        console.log(i)
+    }
+}
