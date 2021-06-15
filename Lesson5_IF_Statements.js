6afa7855d56d4c9713fb4533a3c38fda0b8a6d8c
Serkan <serkara2de@hotmail.com>
2021-06-15 14:59:18 +0200

    If statements added

---
+++ Lesson5_IF_Statements.js
@@ -0,0 +1,33 @@
+
+
+// IF statements
+console.log("before if");
+
+if(5>3){
+    console.log("First Statement")
+}
+
+console.log("After if");
+console.log("================")
+
+var apples, oranges;
+apples = 10;
+oranges=6;
+if(apples>oranges){
+    console.log("I have more Apples than Oranges ");
+};
+console.log("================")
+
+var userName ="Adam";
+
+if(userName=="Adam"){
+    console.log("Welcome "+userName)
+}
+console.log("================")
+
+var books = 12;
+if(books<20){
+    books= books+10;
+}
+console.log(books);
+
