68ce5c3e33970c8987a4bcce6814b5e0c75a3256
Serkan <serkara2de@hotmail.com>
2021-06-16 00:11:35 +0200

    While and DoWhile Loops added

---
+++ Lesson7_WHILE_Loop.js
@@ -0,0 +1,8 @@
+var apples =10;
+
+while(apples>0){
+
+    console.log(apples+" Eating an apple")
+    apples--;
+}
+console.log("No more apples left")
---
+++ Lesson8_DO_WHILE_Loop.js
@@ -0,0 +1,9 @@
+var apples = 10;
+do{
+    console.log(apples + " Eating an apple")
+    apples--;
+}while (apples > 0) {
+}
+console.log("No more apples left")
+
+console.log("================")
